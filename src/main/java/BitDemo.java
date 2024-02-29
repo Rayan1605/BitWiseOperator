fd02e7dd3b3e73cc96b1bc7ef2a796282fc6cf38
rayan <rayanzyad99@gmail.com>
2024-02-29 19:05:43 +0000

    Start

--- src/main/java/BitDemo.java
+++ src/main/java/BitDemo.java
@@ -14,7 +14,9 @@ public class BitDemo {
 // bit of its second operand. If both bits are 1,
 // the corresponding result bit is set to 1. Otherwise, it is set to 0.
 
-/// Unsigned right shift
+/// Unsigned right shift // Shifts the bits of the first operand to the right by the number of
+// positions specified by the second operand.
+
 // Signed right shift
 // Signed left shift
 // ~ Unary bitwise complement // all the 0 bits become 1s, and all the 1 bits become 0s.
