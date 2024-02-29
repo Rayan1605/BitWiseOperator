e2ec07479b898a1e1248c56ffebab35d82b11ee6
rayan <rayanzyad99@gmail.com>
2024-02-29 19:01:36 +0000

    Start

--- src/main/java/BitDemo.java
+++ src/main/java/BitDemo.java
@@ -5,7 +5,11 @@ public class BitDemo {
 // | Bitwise inclusive OR // Compares each bit of its first operand to the corresponding bit of
 // its second operand. If either bit is 1, the corresponding result bit is set to 1. Otherwise,
 // it is set to 0.
-// ^ Bitwise exlusive OR
+
+// ^ Bitwise exlusive OR // Compares each bit of its first operand to the corresponding bit of its
+// second operand. If the bits are different, the corresponding result bit is set to 1.
+// If the bits are the same, the result bit is set to 0.
+
 // & Bitwise AND
 /// Unsigned right shift
 // Signed right shift
