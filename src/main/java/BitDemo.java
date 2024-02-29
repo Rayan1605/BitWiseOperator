c6361049f1f76cb35bb002f580ab44ea74518f2a
rayan <rayanzyad99@gmail.com>
2024-02-29 18:57:35 +0000

    Start

--- src/main/java/BitDemo.java
+++ src/main/java/BitDemo.java
@@ -1,2 +1,13 @@
 public class BitDemo {
 }
+
+//Bitwise and Bit Shift Operator
+// | Bitwise inclusive OR // Compares each bit of its first operand to the corresponding bit of
+// its second operand. If either bit is 1, the corresponding result bit is set to 1. Otherwise,
+// it is set to 0.
+// ^ Bitwise exlusive OR
+// & Bitwise AND
+/// Unsigned right shift
+// Signed right shift
+// Signed left shift
+// ~ Unary bitwise complement // all the 0 bits become 1s, and all the 1 bits become 0s.
